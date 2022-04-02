115aa9e0ebb35245de9ca19070ed3507a069cd05
Ayudesee <sharaevvlad@mail.ru>
2022-04-02 21:17:47 +0300

    added classes hmw14

--- hmw14/client.py
+++ hmw14/client.py
@@ -1,36 +1,39 @@
 import socket
 
 
-SRV_HOST = "127.0.0.1"
-SRV_PORT = 6969
+class Client:
+    def __init__(self):
+        self.SRV_HOST = "127.0.0.1"
+        self.SRV_PORT = 6969
+        self.username = input("Please, enter your username: ")
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.SRV_HOST, self.SRV_PORT))
+        self.kw_refresh = ("r", "refresh")
+        self.kw_quit = (r"\q", r"\quit", r"\exit")
 
-username = input("Please, enter your username: ")
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SRV_HOST, SRV_PORT))
-kw_refresh = ("r", "refresh")
-kw_quit = (r"\q", r"\quit", r"\exit")
+    def close_connection(self):
+        self.send_message(f"{self.username} has left the chat")
+        self.client.close()
+
+    def send_message(self, msg):
+        self.client.send(f"{self.username}: {msg}".encode())
+
+    def receive_message(self):
+        print(self.client.recv(2048).decode())
+
+    def start(self):
+        self.client.send(f"'{self.username}' joined the chat".encode())
+        while True:
+            message = input(f"{self.username}: ")
+            if message in self.kw_refresh:
+                self.receive_message()
+            elif message in self.kw_quit:
+                self.close_connection()
+                break
+            else:
+                self.send_message(message)
 
 
-def close_connection(clsock):
-    send_message(clsock, f"{username} has left the chat")
-    clsock.close()
-
-
-def send_message(clsock, msg):
-    clsock.send(f"{username}: {msg}".encode())
-
-
-def receive_message(clsock):
-    print(clsock.recv(2048).decode())
-
-
-client.send(f"'{username}' joined the chat".encode())
-while True:
-    message = input(f"{username}: ")
-    if message in kw_refresh:
-        receive_message(client)
-    elif message in kw_quit:
-        close_connection(client)
-        break
-    else:
-        send_message(client, message)
+if __name__ == '__main__':
+    cl1 = Client()
+    cl1.start()
--- hmw14/server.py
+++ hmw14/server.py
@@ -1,56 +1,59 @@
 import socket
 import select
 
-FOR_READ = []
-FOR_WRITE = []
 
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 6969
+class Server:
+    def __init__(self):
+        self.FOR_READ = []
+        self.FOR_WRITE = []
+        self.SERVER_HOST = "127.0.0.1"
+        self.SERVER_PORT = 6969
+        self.srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._srv_socket_settings()
+        self.BUFFER = {}
+        self.CONNECTED_CLIENTS = []
 
-srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-srv_sock.bind((SERVER_HOST, SERVER_PORT))
-srv_sock.listen(20)
+    def _srv_socket_settings(self):
+        self.srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.srv_sock.bind((self.SERVER_HOST, self.SERVER_PORT))
+        self.srv_sock.listen(20)
+        self.srv_sock.setblocking(False)
+        self.FOR_READ.append(self.srv_sock)
 
-srv_sock.setblocking(False)
-FOR_READ.append(srv_sock)
+    def delete_connected_client(self, rsock):
+        for index, c in enumerate(self.CONNECTED_CLIENTS):
+            if rsock == c:
+                del self.CONNECTED_CLIENTS[index]
 
-BUFFER = {}
-CONNECTED_CLIENTS = []
-
-
-def delete_connected_client(rsock):
-    for index, c in enumerate(CONNECTED_CLIENTS):
-        if rsock == c:
-            del CONNECTED_CLIENTS[index]
-
-
-def send_message_to_clients(rsock):
-    data = rsock.recv(2048).decode()
-    # if client socket is closed
-    if not data:
-        delete_connected_client(rsock)
-    else:
-        print(data)
-    # send message to all clients but sender
-    for clientsock in CONNECTED_CLIENTS:
-        if rsock != clientsock:
-            clientsock.send(data.encode())
-
-
-def append_client():
-    client, addr = srv_sock.accept()
-    client.setblocking(False)
-    FOR_READ.append(client)
-    CONNECTED_CLIENTS.append(client)
-
-
-print("Server started")
-# main loop
-while True:
-    R, W, ERR = select.select(FOR_READ, FOR_WRITE, FOR_READ)
-    for r in R:
-        if r is srv_sock:
-            append_client()
+    def send_message_to_clients(self, rsock):
+        data = rsock.recv(2048).decode()
+        # if client socket is closed
+        if not data:
+            self.delete_connected_client(rsock)
         else:
-            send_message_to_clients(r)
+            print(data)
+        # send message to all clients but sender
+        for clientsock in self.CONNECTED_CLIENTS:
+            if rsock != clientsock:
+                clientsock.send(data.encode())
+
+    def append_client(self):
+        client, addr = self.srv_sock.accept()
+        client.setblocking(False)
+        self.FOR_READ.append(client)
+        self.CONNECTED_CLIENTS.append(client)
+
+    def start(self):
+        print("Server started")
+        while True:
+            R, W, ERR = select.select(self.FOR_READ, self.FOR_WRITE, self.FOR_READ)
+            for r in R:
+                if r is self.srv_sock:
+                    self.append_client()
+                else:
+                    self.send_message_to_clients(r)
+
+
+if __name__ == "__main__":
+    srv = Server()
+    srv.start()
