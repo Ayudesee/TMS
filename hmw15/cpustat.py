c053f7aa2b422e5b19dc91ecbfed1c2506a80aeb
Ayudesee <sharaevvlad@mail.ru>
2022-04-06 20:21:30 +0300

    shitty decorator added

--- hmw15/cpustat.py
+++ hmw15/cpustat.py
@@ -4,6 +4,7 @@ import psutil
 import os
 from colorama import init
 from colorama import Fore, Back, Style
+from decorators import save_to_file
 
 
 class CPUStat:
@@ -16,7 +17,7 @@ class CPUStat:
 
     def get_all_processes(self, user="usr") -> dict:
         """Get all processes running on machine with user='all'.
-         Get processes without 'None' user with user='usr'"""
+         Get processes without user 'None' with user='usr'"""
 
         proc_dict = {}
         for proc in psutil.process_iter():
@@ -25,6 +26,7 @@ class CPUStat:
                 continue
             proc_dict[proc.pid] = dictproc
         return proc_dict
+
 
     def virtual_memory(self):
         return psutil.virtual_memory()
@@ -66,25 +68,45 @@ class CPUStat:
         astnum = int(n * round(percent / n) / 5)
         return f"({Fore.RED + '*' * astnum}{Fore.GREEN + '-' * (20 - astnum) + Style.RESET_ALL})"
 
+    @save_to_file
     def print_disks(self, kwargs):
+        return_list = []
         for disk in kwargs['all_disks']:
             diskinfo = self.get_disk_usage(disk.mountpoint)
             percent_bar = f"{diskinfo.percent}% {self.get_percent_bar(diskinfo.percent)}"
-            print(f"{disk.mountpoint:<10} Total: {diskinfo.total / 1024 / 1024 / 1024:.0f} GB {percent_bar:>50}")
+            on_print = f"{disk.mountpoint:<10} Total: {diskinfo.total / 1024 / 1024 / 1024:.0f} GB {percent_bar:>50}"
+            return_list.append(on_print)
+            print(on_print)
+        return return_list
 
+    @save_to_file
     def print_boot_time(self, kwargs):
-        print(f"Boot time: " + kwargs['boot_time'])
+        return_list = []
+        on_print = f"Boot time: " + kwargs['boot_time']
+        return_list.append(on_print)
+        print(on_print)
+        return return_list
 
+    @save_to_file
     def print_ram(self, kwargs):
+        return_list = []
         ram = f"RAM: {kwargs['svmem'].total / 1024 / 1024 / 1024:.0f} GB"
         svmem_perc = f"{kwargs['svmem'].percent}% {self.get_percent_bar(kwargs['svmem'].percent)}"
-        print(f"{ram:<10}{svmem_perc:>65}")
+        on_print = f"{ram:<10}{svmem_perc:>65}"
+        return_list.append(on_print)
+        print(on_print)
+        return return_list
 
+    @save_to_file
     def print_processes(self, kwargs):
+        return_list = []
         for proc, val in kwargs['processes'].items():
             username = val['username'].split('\\')[-1]
             process_name = val['name']
-            print(f"{proc:<10}", f"{username:>14}", f"{process_name:>35}")
+            on_print = f"{proc:<10} {username:>14} {process_name:>35}"
+            return_list.append(on_print)
+            print(on_print)
+        return return_list
 
     def formatter(self, **kwargs):
         self.print_disks(kwargs)
